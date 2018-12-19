4ff2515f43209f28b973ea0da48577570005f887
yangleih <yangleih@yonyou.com>
2018-12-19 15:38:35 +0800

    all files

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+/**
+ * Created by Administrator on 2015/9/11.
+ */
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+    //Project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify:{// 压缩js的配置
+            options:{
+                banner:'/*! create by <%= grunt.template.today("yyyy-mm-dd")%>*/\n'
+            },
+            static_mappings:{
+                files:[{
+                    src:'js/index.js',
+                    dest:'build/index.min.js'
+                }]
+            }
+
+        },
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['babel-preset-es2015']
+            },
+            dist: {
+                files: {
+                    'js/index.js': 'dest/index.js'
+                }
+            }
+        },
+        concat:{//合并js配置
+            bar:{
+                src:['build/*.js'],
+                dest:'dest/all.min.js'
+            }
+        },
+        watch:{//自动监听任务
+            files:['js/*.js'],
+            tasks:['uglify','concat']
+        },
+        jshint:{//检查js的错误
+            options: {
+                newcap:false
+            },
+            files: ['Gruntfile.js', 'js/*.js'],
+        }
+    });
+    //加载包含“uglify"任务插件
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
+    // grunt.loadNpmTasks('grunt-contrib-concat');
+    // grunt.loadNpmTasks('grunt-contrib-watch');
+     grunt.loadNpmTasks('grunt-contrib-jshint');
+     grunt.loadNpmTasks('grunt-babel');
+
+    //默认被执行的任务列表
+    grunt.registerTask('default', ['jshint','uglify','babel','concat','watch']);
+};
---
+++ js/index.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Administrator on 2015/9/11.
+ */
+(function (global) {
+
+    'use strict';
+
+     // Default impl options
+     var defaultOptions = {
+        // Default is to fail on error, no placeholder
+        imagePlaceholder: undefined,
+        // Default cache bust is false, it will use the cache
+        cacheBust: false
+    };
+
+
+    var yardCode = {
+        barcode: barCode
+    };
+
+    if (typeof module !== 'undefined')
+        module.exports = yardCode;
+    else
+        global.yardCode = yardCode;
+    
+    
+    function barCode(type,val,options){
+        if(type===1002){
+            jsBarcodes(val);
+        }else{
+            qRiouss(val,options);
+        }
+    }
+
+    function jsBarcodes(val){
+        var canvas = document.createElement("canvas");
+        JsBarcode(canvas, val, {format: "CODE39"});
+        return canvas.toDataURL("image/png");
+    }
+
+    function qRiouss(val,options){
+        var obj = Object.assign({value: val},options?options:{
+            background:background,
+            backgroundAlpha: 0.8,
+            foreground: foreground,
+            foregroundAlpha: 0.8,
+            level: 'H',
+            padding: padding,
+            size: size,
+        });
+       return new QRious(obj).toDataURL('image/jpeg');
+    }
+
+})(this);
